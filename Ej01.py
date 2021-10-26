336ab8024d4d6e37320b35c7db09cfa6fef937a9
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2021-10-26 23:03:57 +0200

    realizando el pseudocódigo

--- Ej01.py
+++ Ej01.py
@@ -1,5 +1,14 @@
 #calcular la distancia que recorre ujn objeto con una velocidad y un timepo determinado
 
+
+#PSEUDOCÓDIGO
+
+# para relaizar le código del enunciado, lo primero que necesitamos es establecer las variables(velocidad, timepo y distancia)
+# y establecer la relacion que las une, en este caso al ser un movimiento rectilíneor uniforme, la ecuación sera:
+# distancia=tiempo*velocidad
+# para luego poder concatenar el prodecto conuna cadena de texto para imprimirla,
+# la transformaremos a un string y mostraremos mediante una función print el resultado
+
 #en metros por segundo
 velocidad=2
 
